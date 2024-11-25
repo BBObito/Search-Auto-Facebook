0021e3cdeb21d2e56d45602b234e0dd98b27d5c6
KyTranDE <21011801.ky@student.iuh.edu.vn>
2024-11-25 10:28:00 +0700

    feat: update requirements.txt with selenium and python-dotenv; remove obsolete list-url.txt; refactor src.py and logger.py to clean up unused functions and improve cookie management

--- api/src/src.py
+++ api/src/src.py
@@ -171,39 +171,6 @@ def get_element_coordinates(driver: webdriver.Chrome):
 
     except Exception as e:
         logger.logger('logs/error.log', f"Error getting element coordinates: {e}")
-
-def click_see_more_buttons(driver: webdriver.Chrome) -> None:
-    """
-    Click vào các nút 'See More' để tải thêm kết quả.
-    Args:
-        driver (webdriver.Chrome): Đối tượng driver.
-    Returns:
-        None
-    """
-    while True:
-        try:
-            see_more_button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'See More') or contains(text(), 'More')]"))
-            )
-            see_more_button.click()
-            time.sleep(2)
-        except Exception as e:
-            logger.logger('logs/error.log', f"No more 'See More' buttons found or error occurred: {e}")
-            break
-
-def find_All_xpath(driver: webdriver.Chrome, name_file: str) -> None:
-    """
-    """
-    xpath_target = driver.find_elements(By.XPATH, '//*[starts-with(@id, ":r")]/span/span/a[@role="link" and @tabindex="0"]')
-    file_path = f'results/{date_folder}/{name_file}.txt'
-    with open(file_path, 'a', encoding='utf-8') as file:
-        for element in xpath_target:
-            url = element.get_attribute('href')
-            if url:
-                file.write(url + '\n')
-                logger.logger('logs/info.log', f"Saved URL: {url}")
-            else:
-                logger.logger('logs/error.log', f"Failed to retrieve URL from element: {element}")
                 
 def filter_pages(driver: webdriver.Chrome, stop_k: int) -> bool:
     """
@@ -213,7 +180,6 @@ def filter_pages(driver: webdriver.Chrome, stop_k: int) -> bool:
     Returns:
         bool: True nếu lọc thành công, ngược lại False.    
     """
-    # time.sleep(500)
     try:
         pages_filter = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Posts')]"))
@@ -228,7 +194,6 @@ def filter_pages(driver: webdriver.Chrome, stop_k: int) -> bool:
         
         
         scroll_to_load_all_results(driver,stop_k=stop_k)
-        
         check_duplicate_urls()
         return True
     except Exception as e:
@@ -278,14 +243,6 @@ def perform_search(search_query,passwword,stop_k) -> None:
         logger.logger('logs/info.log', f"Crawling {search_query} ...")
 
     driver.quit()
-
-def search(q,password,stop_k) -> None:
-    """
-    Đọc các từ khóa từ file và thực hiện tìm kiếm.
-    Returns:
-        None
-    """
-    perform_search(q,password,stop_k)
     
 def check_login(email, password):
     """
@@ -293,6 +250,19 @@ def check_login(email, password):
     Returns:
         None
     """
+    
+    file_path = "facebook_cookies.pkl"
+    if os.path.exists(file_path):
+        creation_time = os.path.getctime(file_path)
+        readable_time = time.ctime(creation_time)
+        # lấy time hiện tại trù đi time của readable_time nếu mà lớn hơn 2h thì xóa file
+        if time.time() - creation_time > 7200:
+            os.remove(file_path)
+            print("File đã bị xóa.")
+            
+    else:
+        print("File không tồn tại.")
+        
     if not os.path.exists('facebook_cookies.pkl'):
         get_cookies(email=email, password=password)
 
--- api/src/utils/logger.py
+++ api/src/utils/logger.py
@@ -334,3 +334,43 @@ def logger(path_log, text):
 # #                 logger.logger('logs/error.log', f"Error processing article: {inner_e}")
 # #                 continue
 
+# def click_see_more_buttons(driver: webdriver.Chrome) -> None:
+#     """
+#     Click vào các nút 'See More' để tải thêm kết quả.
+#     Args:
+#         driver (webdriver.Chrome): Đối tượng driver.
+#     Returns:
+#         None
+#     """
+#     while True:
+#         try:
+#             see_more_button = WebDriverWait(driver, 10).until(
+#                 EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'See More') or contains(text(), 'More')]"))
+#             )
+#             see_more_button.click()
+#             time.sleep(2)
+#         except Exception as e:
+#             logger.logger('logs/error.log', f"No more 'See More' buttons found or error occurred: {e}")
+#             break
+
+# def find_All_xpath(driver: webdriver.Chrome, name_file: str) -> None:
+#     """
+#     """
+#     xpath_target = driver.find_elements(By.XPATH, '//*[starts-with(@id, ":r")]/span/span/a[@role="link" and @tabindex="0"]')
+#     file_path = f'results/{date_folder}/{name_file}.txt'
+#     with open(file_path, 'a', encoding='utf-8') as file:
+#         for element in xpath_target:
+#             url = element.get_attribute('href')
+#             if url:
+#                 file.write(url + '\n')
+#                 logger.logger('logs/info.log', f"Saved URL: {url}")
+#             else:
+#                 logger.logger('logs/error.log', f"Failed to retrieve URL from element: {element}")
+
+# def search(q,password,stop_k) -> None:
+#     """
+#     Đọc các từ khóa từ file và thực hiện tìm kiếm.
+#     Returns:
+#         None
+#     """
+#     perform_search(q,password,stop_k)
